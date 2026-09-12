ac8bdb42555e4e1ac984eba8f269f8a7150deb84
saifur25 <muhammadsaifur25@gmail.com>
2026-09-12 19:19:08 +0600

    daisyUI: light option has been added on HTML file and technologies section has been redesigned

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar"
 import Techs from "./components/techStacks/Techs"
 
 
+
 function App() {
 
   return (
--- src/components/techStacks/Techs.tsx
+++ src/components/techStacks/Techs.tsx
@@ -3,8 +3,8 @@
 const Techs = () => {
     return (
         <div className="container mx-auto">
-            <h2>Explore the <span>Technologies</span> </h2>
-            <p>Pick one technology per category to build your ideal stack.</p>
+            <h2 className="text-4xl font-bold">Explore the <span className="text-[#CE4EB6] ">Technologies</span> </h2>
+            <p className="text-2xl">Pick one technology per category to build your ideal stack.</p>
         </div>
     )
 }
