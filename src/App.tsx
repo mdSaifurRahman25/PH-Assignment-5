cb626084e0bacd8e2d1f9d71afb458e739b66d3e
saifur25 <muhammadsaifur25@gmail.com>
2026-09-12 19:12:24 +0600

    Technologies section and json data is added successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Banner from "./components/Banner"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
+import Techs from "./components/techStacks/Techs"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar />
       <Banner />
+      <Techs />
       <Footer />
     </>
   )
---
+++ src/components/techStacks/Techs.tsx
@@ -0,0 +1,12 @@
+
+
+const Techs = () => {
+    return (
+        <div className="container mx-auto">
+            <h2>Explore the <span>Technologies</span> </h2>
+            <p>Pick one technology per category to build your ideal stack.</p>
+        </div>
+    )
+}
+
+export default Techs
