d54e827549eb56337f3fdb3c738da6ce299ce4f3
saifur25 <muhammadsaifur25@gmail.com>
2026-09-12 19:05:42 +0600

    footer section added successfully

---
+++ src/components/Footer.tsx
@@ -0,0 +1,65 @@
+import Logo from '../assets/logo-text.png'
+
+const Footer = () => {
+    return (
+        <div className="border-t border-gray-200">
+            <div className='container mx-auto grid grid-cols-12 my-15'>
+                {/* left Logo Section */}
+                <div className='col-span-5'>
+                    <div className='mb-4'>
+                        <img src={Logo} alt="Website Logo" />
+                    </div>
+                    <h5 className='mb-5'>Curated tools, software, and resources for developers building modern software.</h5>
+                    <ul className='flex gap-4 font-bold'>
+                        <li>GitHub</li>
+                        <li>Twitter</li>
+                        <li>LinkedIn</li>
+                    </ul>
+                </div>
+
+                {/* Right 3 Link Section */}
+                <div className='col-span-7'>
+                    <div className='grid grid-cols-6'>
+                        <div className='col-span-2'>
+                            <h5 className='font-bold mb-4'>PRODUCT</h5>
+                            <ul className='text-gray-500'>
+                                <li>Home</li>
+                                <li>Technologies</li>
+                                <li>Projects</li>
+                            </ul>
+                        </div>
+                        <div className='col-span-2'>
+                            <h5 className='font-bold mb-4'>COMPANY</h5>
+                            <ul className='text-gray-500'>
+                                <li>About</li>
+                                <li>Contact</li>
+                                <li>Careers</li>
+                            </ul>
+                        </div>
+                        <div className='col-span-2'>
+                            <h5 className='font-bold mb-4'>LEGAL</h5>
+                            <ul className='text-gray-500'>
+                                <li>Privacy Policy</li>
+                                <li>Terms of Services</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Additional Footer Bottom */}
+            <div className='container mx-auto border-t border-gray-200'>
+                <div className='flex justify-between my-9 text-gray-400'>
+                    <div>
+                        <p>&copy; 2026 DevStack. All rights reserved.</p>
+                    </div>
+                    <div className='flex gap-4'>
+                        <p className="cursor-pointer hover:text-black transition-colors">Privacy</p>
+                        <p className="cursor-pointer hover:text-black transition-colors">Terms</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,13 @@ const Navbar = () => {
     return (
         <div className=' border-b border-gray-200 py-[20px]'>
             <div className='container mx-auto flex justify-between items-center'>
+                
+                {/* Logo */}
                 <div>
                     <img src={Logo} alt="" />
                 </div>
+
+                {/* Menu */}
                 <ul className='flex gap-6 text-gray-600 font-medium'>
                     <li className='text-[#E51E7E]'>Home</li>
                     <li>Technologies</li>
@@ -14,6 +18,8 @@ const Navbar = () => {
                     <li>About</li>
                     <li>Contact</li>
                 </ul>
+
+                {/* Button */}
                 <div className='flex gap-5 items-center text-gray-600 font-medium'>
                     <button>Sign In</button>
                     <button className="bg-[#E51E7E] text-white px-6 py-2.5 rounded-full font-medium hover:bg-[#d0176e] transition-colors">
