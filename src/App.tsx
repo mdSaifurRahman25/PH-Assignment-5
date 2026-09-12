d49aebfe0a1f47d6be1b03c4bc31d93b5c2a354b
saifur25 <muhammadsaifur25@gmail.com>
2026-09-12 12:36:27 +0600

    react, tailwind css, daisyUI set up done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h2 className='bg-red-500'>Hello World</h2>
+      <button className="btn btn-neutral">Neutral</button>
+    </>
+  )
+}
+
+export default App
